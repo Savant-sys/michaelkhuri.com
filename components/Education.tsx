505759f14dc4253ba00f23ab93361be596384229
Savant-sys <michaelkhuri@gmail.com>
2024-10-01 15:02:03 -0700

    Update Education.tsx

--- components/Education.tsx
+++ components/Education.tsx
@@ -96,7 +96,7 @@ const Education: React.FC<{}> = () =>
           <p className="text-gray-300 md:pt-0 pt-3">Jun 2020 - May 2022</p>
         </motion.div>
         <motion.p className="text-gray-300 pt-5" variants={fadeInUp}>
-          • Though my degree in Liberal Arts, my primary focus was on Computer Science coursework.
+          • Though my degree is in Liberal Arts, my primary focus was on Computer Science coursework.
         </motion.p>
 
         <motion.p className="text-gray-300 pt-5" variants={fadeInUp}>
