d784a2759e1c50344f4c23582025bb8c559c8261
Michael Khuri <michaelkhuri@gmail.com>
2025-04-02 15:20:35 -0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -71,7 +71,7 @@ const Footer: React.FC<{}> = () =>
       </motion.div>
 
       <motion.p className="text-gray-300 text-center text-xs md:text-sm" variants={fadeInUp}>
-        Created with 💜 using Next.js © {new Date().getFullYear()}
+        Created with 🩶 using Next.js © {new Date().getFullYear()}
       </motion.p>
     </motion.div>
   );
